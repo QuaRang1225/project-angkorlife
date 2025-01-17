5ef4058558f030bcfd2f12fb487516ed95e77257
QuaRang <dbduddnd1225@gmail.com>
2025-01-18 06:52:29 +0900

    #2 - 로그인 관련 기능 정의
    UserDefaultsManager
    - 로그인 메서드 정의
    - 로그인이 되었는지 유무를 판단하는 메서드 정의
    - 테스트 코드 작성 및 UnitTest진행

---
+++ project-angkorlife/Manager/UserDefaultsManager.swift
@@ -0,0 +1,27 @@
+//
+//  UserDefaultsManager.swift
+//  project-angkorlife
+//
+//  Created by 유영웅 on 1/18/25.
+//
+
+import Foundation
+
+class UserDefaultsManager{
+    static let instance = UserDefaultsManager()
+    private init(){}
+    
+    let userId = "user_id"  //User Defaults키
+    
+    //로그인 유무를 판단하여 반환하는 메서드
+    //true - 로그인됨 / false - 로그인 필요
+    func isUserLoggedIn()->Bool{
+        guard UserDefaults.standard.string(forKey:userId) == nil else { return true }
+        return false
+    }
+    //로그인 메서드
+    //id를 User Defaults에 저장
+    func signIn(id:String){
+        UserDefaults.standard.set(id,forKey:userId)
+    }
+}
--- project-angkorlifeTests/project_angkorlifeTests.swift
+++ project-angkorlifeTests/project_angkorlifeTests.swift
@@ -13,5 +13,9 @@ struct project_angkorlifeTests {
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
+    @available(iOS 15.0, *)
+    @Test func testIsUserLoggedIn() async throws {
+        let isLoggedIn = UserDefaultsManager.instance.isUserLoggedIn()
+        #expect(isLoggedIn == true)
+    }
 }
