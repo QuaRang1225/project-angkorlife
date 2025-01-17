143067e1aa2abde0e291c3364bcb6d259b3d0768
QuaRang <dbduddnd1225@gmail.com>
2025-01-18 08:12:03 +0900

    #1 - 이미지 적용
    - Figma에서 배경화면에 적용할 이미지 추출 후 적용
    - 로그인 뷰 생성

---
+++ project-angkorlife/Core/Authentication/View/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  project-angkorlife
+//
+//  Created by 유영웅 on 1/18/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
