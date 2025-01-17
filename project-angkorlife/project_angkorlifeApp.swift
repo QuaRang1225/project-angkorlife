033d66c5a73cb20f603b7b130ee217a13b836a56
QuaRang <dbduddnd1225@gmail.com>
2025-01-18 01:34:21 +0900

    add project

---
+++ project-angkorlife/project_angkorlifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_angkorlifeApp.swift
+//  project-angkorlife
+//
+//  Created by 유영웅 on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct project_angkorlifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ project-angkorlifeTests/project_angkorlifeTests.swift
@@ -0,0 +1,17 @@
+//
+//  project_angkorlifeTests.swift
+//  project-angkorlifeTests
+//
+//  Created by 유영웅 on 1/18/25.
+//
+
+import Testing
+@testable import project_angkorlife
+
+struct project_angkorlifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
