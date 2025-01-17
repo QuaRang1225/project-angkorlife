2850326aa3c2bb21e96633f96d3090608bef6e05
QuaRang <dbduddnd1225@gmail.com>
2025-01-18 08:15:12 +0900

    #2 - 로그인 화면 디자인
    - 로그인 화면에 필요한 이미지 레이아웃 배치
    - TextField와 버튼 배치
    - 폰트 및 프레임 크기 조정

--- project-angkorlife/Core/Authentication/View/SignInView.swift
+++ project-angkorlife/Core/Authentication/View/SignInView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct SignInView: View {
+    
+    @State var id = ""
+    private let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+    
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            Image("IMG_MAIN")
+                .resizable()
+                .frame(height:windowScene?.screen.bounds.width)
+            TextField("Enter your ID",text: $id)
+                .padding()
+                .background{
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(lineWidth: 1)
+                        .foregroundStyle(.gray)
+                    RoundedRectangle(cornerRadius: 5)
+                        .foregroundStyle(.gray.opacity(0.1))
+                }
+                .padding(10)
+                .foregroundStyle(.white)
+            Button{
+                
+            }label:{
+                Capsule()
+                    .foregroundStyle(.indigo)
+                    .frame(height:55)
+                    .overlay {
+                        Text("Log in")
+                            .foregroundStyle(.white)
+                            .font(.KantumruyProBold(18))
+                    }
+            }
+            .padding(10)
+            Spacer()
+            Image("IMG_EARTH")
+                .resizable()
+                .frame(height:(windowScene?.screen.bounds.width ?? 0)/3)
+                
+        }
+        .ignoresSafeArea()
+        .background(.black)
     }
 }
+
 
 #Preview {
     SignInView()
