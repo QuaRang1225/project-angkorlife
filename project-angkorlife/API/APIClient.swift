a59b9943fe18452a65ecde45d8cc1fd296589a4c
QuaRang <dbduddnd1225@gmail.com>
2025-01-20 00:45:38 +0900

    #6 - API 클라이언트 및 모니터 구현
    - 각각의 기능 구현
    - Client: API 요청을 처리하는 중앙 네트워크 클라이언트 생성
    - Monitor: 네트워크 상태 변화를 감지하기 위한 네트워크 모니터링 도구

--- project-angkorlife/API/APIClient.swift
+++ project-angkorlife/API/APIClient.swift
@@ -10,14 +10,14 @@ import Alamofire
 
 final class ApiClient{
     
-//    var session:Session
+    var session:Session
     static let shared = ApiClient()
-    static let baseURL =  "\(Bundle.main.infoDictionary?["BASE_URL"] ?? "")"
+    static let baseURL =  "\(Bundle.main.infoDictionary?["BASE_URL"] ?? "")"    //baseURL 설정
     
-//    let monitors = [ApiLogger()] as [EventMonitor]
+    let monitors = [APIMonitor()] as [EventMonitor]                             //이벤트 모니터 추가
     
-    init(){
-//        session = Session(eventMonitors: monitors)
+    private init(){
+        session = Session(eventMonitors: monitors)
     }
     
 }
