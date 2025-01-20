b638c0bc173206692a877cfd8eb752df56ca927a
QuaRang <dbduddnd1225@gmail.com>
2025-01-20 12:56:52 +0900

    #7 - CustomData 클래스 파일 생성
    - 투표자 프로필 이미지 미리보기를 위해 image property 작성

---
+++ project-angkorlife/Core/Custom/CustomData.swift
@@ -0,0 +1,17 @@
+//
+//  CustomData.swift
+//  project-angkorlife
+//
+//  Created by 유영웅 on 1/20/25.
+//
+
+import Foundation
+
+class CustomData{
+    static var instance = CustomData()
+    private init(){}
+    
+    var image:String{
+        "https://avatars.githubusercontent.com/u/31721255?v=4&size=64"
+    }
+}
