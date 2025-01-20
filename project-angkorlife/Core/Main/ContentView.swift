f2a416d6ba9f93f24fa7bb024c0de0257e7ea391
QuaRang <dbduddnd1225@gmail.com>
2025-01-21 00:35:08 +0900

    #11 - 메인화면 코드 모듈화
    - 각 컴포넌트별로 코드 정리

--- project-angkorlife/Core/Main/ContentView.swift
+++ project-angkorlife/Core/Main/ContentView.swift
@@ -12,34 +12,11 @@ struct ContentView: View {
     @StateObject var vm = VoteViewModel()
     var body: some View {
         ZStack(alignment: .topTrailing){
-            NavigationView{
-                ScrollView(showsIndicators: false){
-                    VStack(spacing:0){
-                        TimerView()
-                        InformationView()
-                        VotingParticipantsListView()
-                    }
-                }
-                .background(Color.black) 
-                .ignoresSafeArea()
-            }
-            Button {
-                vm.userId = nil
-                vm.candidateList = nil
-                vm.votedCandidateList.removeAll()
-                isLoggedIn = true
-                UserDefaultsManager.instance.deleteUserId()
-            } label: {
-                Text("Log out")
-                    .foregroundStyle(.white)
-                    .font(.KantumruyProBold(20))
-                    .padding()
-            }
+            navigationView
+            logoutButtonView
         }
         .environmentObject(vm)
-        .onAppear{
-            isLoggedIn = vm.userId == nil
-        }
+        .onAppear{ isLoggedIn = vm.userId == nil }
         .fullScreenCover(isPresented:$isLoggedIn){
             SignInView()
                 .environmentObject(vm)
@@ -49,4 +26,38 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+extension ContentView{
+    //메인화면
+    var navigationView:some View{
+        NavigationView{
+            ScrollView(showsIndicators: false){
+                VStack(spacing:0){
+                    TimerView()
+                    InformationView()
+                    VotingParticipantsListView()
+                }
+            }
+            .background(Color.black)
+            .ignoresSafeArea()
+        }
+    }
+    //로그아웃
+    //로그아웃 시 후보자 리스트 관련 내용 삭제
+    //유저ID 디바이스 내부에서 삭제
+    var logoutButtonView:some View{
+        Button {
+            vm.userId = nil
+            vm.candidateList = nil
+            vm.votedCandidateList.removeAll()
+            isLoggedIn = true
+            UserDefaultsManager.instance.deleteUserId()
+        } label: {
+            Text("Log out")
+                .foregroundStyle(.white)
+                .font(.KantumruyProBold(20))
+                .padding()
+        }
+    }
 }
