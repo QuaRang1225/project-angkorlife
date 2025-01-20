7f758df7571237d7b481b02b92c23b9d037df310
QuaRang <dbduddnd1225@gmail.com>
2025-01-20 15:42:28 +0900

    #6 - ViewModel에서 각 요청 별 메서드 구현
    - 기존에 테스트를 위해 구현한 fetchCandidateList를 참고해 나머지 요청들도 각각 구현

--- project-angkorlife/Core/Main/Configuration/ViewModel/VoteViewModel.swift
+++ project-angkorlife/Core/Main/Configuration/ViewModel/VoteViewModel.swift
@@ -17,7 +17,7 @@ class VoteViewModel:ObservableObject{
     var error = PassthroughSubject<(String,Bool),Never>()
     var cancel = Set<AnyCancellable>()
     
-    func fetchVoteCandidateList(){
+    func fetchCandidateList(){
         APIService.requset(router: .candidateList)
             .sink { completion in
                 switch completion {
@@ -26,8 +26,47 @@ class VoteViewModel:ObservableObject{
                 case .failure(let error):
                     print("API 호출 실패: \(error.localizedDescription)")
                 }
-            } receiveValue: { (value:VoteCandidateList) in
-                self.voteCandidateList = value
+            } receiveValue: { (value:CandidateList) in
+                self.candidateList = value
+            }.store(in: &cancel)
+    }
+    func fetchCandidate(id:Int,userId:String){
+        APIService.requset(router: .candidate(id: id, userId: userId))
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("API 호출 성공")
+                case .failure(let error):
+                    print("API 호출 실패: \(error.localizedDescription)")
+                }
+            } receiveValue: { (value:Candidate) in
+                self.candidate = value
+            }.store(in: &cancel)
+    }
+    func sendVote(userId:String,id:Int){
+        APIService.requset(router: .vote(userId: userId, id: id))
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("API 호출 성공 \(completion)")
+                case .failure(let error):
+                    print("API 호출 실패: \(error.localizedDescription)")
+                }
+            } receiveValue: { value in
+                self.error.send((value,!value.isEmpty ? true : false))
+            }.store(in: &cancel)
+    }
+    func fetchVotedCandidateList(){
+        APIService.requset(router: .candidateList)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("API 호출 성공")
+                case .failure(let error):
+                    print("API 호출 실패: \(error.localizedDescription)")
+                }
+            } receiveValue: { (value:[Int]) in
+                self.votedCandidateList = value
             }.store(in: &cancel)
     }
 }
