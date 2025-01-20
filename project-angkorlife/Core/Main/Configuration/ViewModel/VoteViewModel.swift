92ce28650f7184f8b5d1ae3f4ce35be164f1ca49
QuaRang <dbduddnd1225@gmail.com>
2025-01-20 17:27:47 +0900

    #4 - 투표 관련 네트워크 요청 응답 성패 유무 분기 처리 핸들러로 모듈화
    - 응답 성패 유무 분기처리는 모든 요청별로 동일한 기능을 함으로 ResponseCompletionManager로 따로 모듈화를 진행
    - 메세지는 요청별로 다르기 떄문에 텍스트만 파라미터로 받음

--- project-angkorlife/Core/Main/Configuration/ViewModel/VoteViewModel.swift
+++ project-angkorlife/Core/Main/Configuration/ViewModel/VoteViewModel.swift
@@ -20,12 +20,7 @@ class VoteViewModel:ObservableObject{
     func fetchCandidateList(){
         APIService.requset(router: .candidateList)
             .sink { completion in
-                switch completion {
-                case .finished:
-                    print("투표자 리스트 조회 API 호출 성공")
-                case .failure(let error):
-                    print("투표자 리스트 조회 API 호출 실패: \(error.localizedDescription)")
-                }
+                ResponseCompletionManager.instance.compltionHandler(message: "투표자 리스트 조회", completion: completion)
             } receiveValue: { (value:CandidateList) in
                 self.candidateList = value
             }.store(in: &cancel)
@@ -33,12 +28,7 @@ class VoteViewModel:ObservableObject{
     func fetchCandidate(id:Int,userId:String){
         APIService.requset(router: .candidate(id: id, userId: userId))
             .sink { completion in
-                switch completion {
-                case .finished:
-                    print("투표자 프로필 조회 API 호출 성공")
-                case .failure(let error):
-                    print("투표자 프로필 조회 API 호출 실패: \(error.localizedDescription)")
-                }
+                ResponseCompletionManager.instance.compltionHandler(message: "투표자 프로필 조회", completion: completion)
             } receiveValue: { (value:Candidate) in
                 self.candidate = value
             }.store(in: &cancel)
@@ -46,12 +36,7 @@ class VoteViewModel:ObservableObject{
     func sendVote(userId:String,id:Int){
         APIService.requset(router: .vote(userId: userId, id: id))
             .sink { completion in
-                switch completion {
-                case .finished:
-                    print("투표 API 호출 성공 \(completion)")
-                case .failure(let error):
-                    print("투표 API 호출 실패: \(error.localizedDescription)")
-                }
+                ResponseCompletionManager.instance.compltionHandler(message: "투표", completion: completion)
             } receiveValue: { value in
                 self.error.send((value,!value.isEmpty ? true : false))
                 print("눌림\(value) \(!value.isEmpty ? true : false)")
@@ -60,12 +45,7 @@ class VoteViewModel:ObservableObject{
     func fetchVotedCandidateList(userId:String){
         APIService.requset(router: .votedCandidateList(userId: userId))
             .sink { completion in
-                switch completion {
-                case .finished:
-                    print("유저 투표 투표자 프로필 조회 API 호출 성공")
-                case .failure(let error):
-                    print("유저 투표 투표자 프로필 조회 API 호출 실패: \(error.localizedDescription)")
-                }
+                ResponseCompletionManager.instance.compltionHandler(message: "유저 투표 투표자 프로필 조회", completion: completion)
             } receiveValue: { (value:[Int]) in
                 self.votedCandidateList = value
             }.store(in: &cancel)
